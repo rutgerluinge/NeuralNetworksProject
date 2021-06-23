fe39c9b4c95e12a088adb2a79edc1fa55799e7e5
rutgerluinge <rutger@luinge.eu>
2021-06-23 18:08:17 +0200

    W matrix initialized (look at method)

--- ESN/ESN.py
+++ ESN/ESN.py
@@ -1,10 +1,17 @@
 import numpy as np
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import pandas as pd
+import random
 
 # We need sigmoid functions
 # Some ridge regression
 # Randomization of weights
 # Process the input
 # Process the output
+
+connectivity = 1  # percent
+decimals = 3  # weight decimals
 
 
 class ESN:
@@ -17,14 +24,19 @@ class ESN:
 
         # Instantiate the matrixes to all 0 values
         self.reservoir = [0] * reservoir_size
-        self.W = [[0] * reservoir_size] * reservoir_size
+        self.W = [[0 for i in range(reservoir_size)] for j in range(reservoir_size)]
         self.Win = [[0] * input_size] * reservoir_size
         self.Wfb = [[0] * output_size] * reservoir_size
         self.bias = [0] * reservoir_size
         self.Wout = [[0] * reservoir_size] * output_size
         self.output = [0] * output_size
 
+        self.init_W()
+
     # This could be a uniform distribution or a bell curve around 0? Could implement the eigenvalue thing of 1/ev.
+    # ja, een van deze opties: symmetrical uniform, discrete bi-valued, or normal distribution
+    # centered around zero, Gaussian distributions (populair) en uniform distribution (populair)
+    # around 0!!
     def randomize_weights(self, randomization):
         pass
 
@@ -34,5 +46,42 @@ class ESN:
         pass
 
     def get_output(self):
+        # does this need the linear regression?
         self.output = self.Wout * self.reservoir
         return self.output
+
+    def init_W(self):
+        # ja, een van deze opties: symmetrical uniform, discrete bi-valued, or normal distribution
+        # centered around zero, Gaussian distributions (populair) en uniform distribution (populair)
+        # connectivity 1 procent -> 10 connections per neuron
+
+        for i in range(len(self.W)):
+            for j in range(len(self.W)):
+                if random.randint(1, 99) <= connectivity:   #connectivity is set to 1 (0.01 or 1 percent)
+                    self.W[i][j] = round(random.gauss(0, 0.3),
+                                         decimals)  # gaussian distribution, first digit is mean, 2nd standard deviation (not sure bout that)
+
+
+
+    def init_Win(self):
+        pass
+
+    def init_bias(self):
+        pass
+
+    def printW(self):
+        for i in range(self.reservoir_size):
+            print(self.W[i])
+
+
+def ESN_main():
+    Tk().withdraw()
+    filename = askopenfilename()
+    data = pd.read_csv(filename)
+
+    esn = ESN(4, 20, 1,
+              None)  # predict 1 timestamp based on the 4 previous ones? reservoir size = 1000 (might need more)
+
+
+if __name__ == '__main__':
+    ESN_main()

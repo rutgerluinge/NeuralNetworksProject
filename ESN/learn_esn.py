29cf290f6dccf6725ad4eec392cb5494cf0db345
Mattias Kolk <mattiasv.d.kolk@gmail.com>
2021-06-26 19:26:03 +0200

    Added ridge regression and saving of network, still need to look into array.dot()

---
+++ ESN/learn_esn.py
@@ -0,0 +1,59 @@
+from ESN import ESN
+from save_esn import save_esn
+
+from sklearn.linear_model import Ridge
+import pandas as pd
+import numpy as np
+
+import os
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+'''
+-Training data
+-Test data
+-- Or the split between them
+-Data in sizes of days?
+
+- Log the actual outputs? right?
+'''
+
+'''
+data - the data sepperated in wanted lengths
+input-length - the sequence the ESN gets without expecting any results
+'''
+def train_esn(ESN, data, input_length):
+    # Run network
+    data_points = len(data)
+    state_matrix = [None] * (data_points - input_length)
+    for i in range(data_points):
+        if i < input_length:
+            ESN.process_input(data[i])
+        else:
+            ESN.output = data[i - 1]
+            ESN.process_input(0)
+            state_matrix[i-input_length] = ESN.reservoir
+    print(np.array(ESN.reservoir).shape)
+    state_matrix = np.array(state_matrix)
+    print(state_matrix.shape)
+    ESN.Wout = get_weights(state_matrix, data[input_length:])
+
+
+def get_weights(state_matrix, teacher):
+    ridge = Ridge(alpha=1.0)
+    ridge.fit(state_matrix, teacher)
+    return ridge.coef_
+
+
+def learn_main():
+    df = pd.read_csv(BASEDIR + '/../datasets/processed/filled_gabs.csv')
+    data = df['usage'][:10]
+    esn = ESN(1, 500, 1)
+    train_esn(esn, data, 2)
+    save_esn(esn, './esn.txt')
+    #print(esn.Wout)
+
+
+if __name__ == '__main__':
+    learn_main()

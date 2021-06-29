3ed695c70bdebb50d00bf7184f733d9ea177dce0
Mattias Kolk <mattiasv.d.kolk@gmail.com>
2021-06-29 11:21:20 +0200

    Minor changes

--- ESN/learn_esn.py
+++ ESN/learn_esn.py
@@ -4,6 +4,7 @@ from save_esn import save_esn
 from sklearn.linear_model import Ridge
 import pandas as pd
 import numpy as np
+import gc
 
 import os
 
@@ -34,25 +35,33 @@ def train_esn(ESN, data, input_length):
             ESN.output = data[i - 1]
             ESN.process_input(0)
             state_matrix[i-input_length] = ESN.reservoir
-    print(np.array(ESN.reservoir).shape)
     state_matrix = np.array(state_matrix)
-    print(state_matrix.shape)
     ESN.Wout = get_weights(state_matrix, data[input_length:])
 
 
 def get_weights(state_matrix, teacher):
-    ridge = Ridge(alpha=1.0)
+    ridge = Ridge(alpha=0.2)
     ridge.fit(state_matrix, teacher)
     return ridge.coef_
 
 
 def learn_main():
-    df = pd.read_csv(BASEDIR + '/../datasets/processed/filled_gabs.csv')
-    data = df['usage'][:10]
-    esn = ESN(1, 500, 1)
-    train_esn(esn, data, 2)
+    df = pd.read_csv(BASEDIR + '/../datasets/processed/usage_centered.csv')
+    data = df['usage'][:100].copy()
+    del df
+    gc.collect()
+    esn = ESN(1, 1000, 1)
+    train_esn(esn, data, 50)
     save_esn(esn, './esn.txt')
-    #print(esn.Wout)
+    
+    esn.reservoir = [0.0 for i in range(esn.reservoir_size)]
+    for i in range (len(data)):
+        if i < 50:
+            esn.process_input(data[i])
+        else:
+            esn.process_input(0)
+            if i % 10 == 0:
+                print(esn.get_output(), data[i])
 
 
 if __name__ == '__main__':

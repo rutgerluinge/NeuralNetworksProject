4e1aed09a83870e7d9f5d2692bef5dc7b505c5d3
rutgerluinge <rutger@luinge.eu>
2021-07-04 12:33:20 +0200

    comments/cleaned

--- ESN/learn_esn.py
+++ ESN/learn_esn.py
@@ -11,25 +11,11 @@ import os
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-'''
--Training data
--Test data
--- Or the split between them
--Data in sizes of days?
-
-- Log the actual outputs? right?
-'''
-
-'''
-data - the data sepperated in wanted lengths
-input-length - the sequence the ESN gets without expecting any results
-'''
-
 
 # Input: an Echo State Network, the training data, and the input_length that is disregarded for training purposes
 # Puts the training data in the generated ESN
 def train_esn(ESN, data, input_length, alpha):
-    # Run network
+    """Main function that is used for training our ESN"""
     data_points = len(data)
     state_matrix = [None] * (data_points - input_length - 1)
     for i in range(data_points - 1):  # - 1
@@ -46,6 +32,7 @@ def train_esn(ESN, data, input_length, alpha):
 # Output: The fitted weights for the output vector
 # alpha is the regularization strength used in regression
 def get_weights(state_matrix, teacher, alph):
+    """Function that uses ridge regression to generate the fitted weights for Wout based on the reservoir state after training"""
     ridge = Ridge(alpha=alph)
     ridge.fit(state_matrix, teacher)
     return ridge.coef_
@@ -54,17 +41,24 @@ def get_weights(state_matrix, teacher, alph):
 def learn_main():
     df = pd.read_csv(BASEDIR + '/../datasets/processed/processed.csv')
 
-    """Distribution of Data """
-    train_data = df['usage'][:4200].copy()  #data used for training
-    test_data = df['usage'][:4200].copy()   #data used to check if new data is working
-    all_data = df['usage'][:5200].copy()    #data used for independent predictions
+    """Distribution of Data (can be changed) """
+    train_data = df['usage'][:4200].copy()  # data used for training
+    test_data = df['usage'][:4200].copy()  # data used to check if new data is working
+    all_data = df['usage'][:5200].copy()  # data used for independent predictions
+
+    """User Input for hyperparameters"""
+    Win_Scalar = float(input('Enter W-in scalar e.g. (1): '))
+    W_Scalar = float(input('Enter W scalar e.g. (1):'))
+    size = int(input('Enter Neuron Size e.g. (3000): '))
+    leakingRate = float(input('Enter leaking rate e.g. (0.8): '))
+    alpha = float(input('Enter alpha (Ridge Regression) e.g. (1): '))
 
     del df
     gc.collect()
 
     """Creating the Echo State Network and training"""
-    esn = ESN(1, 3000, 1, leaking_rate=0.8, Wscalar=1, WinScalar=1)
-    train_esn(esn, test_data, 2000, 1)
+    esn = ESN(1, size, 1, leaking_rate=leakingRate, Wscalar=W_Scalar, WinScalar=Win_Scalar)
+    train_esn(esn, test_data, 2000, alpha)
 
     output = []
     esn.reservoir = [0.0 for i in range(esn.reservoir_size)]
@@ -96,7 +90,7 @@ def plot(data, trained, cutoff):
     plt.ylabel("Normalized Energy Consumption")
 
     """"Other"""
-    plt.axis([3000, 5000, -1, 1])   #Zoom
+    plt.axis([3000, 5000, -1, 1])  # Zoom
     plt.savefig("EnergyPrecition.png")
     plt.show()
 
